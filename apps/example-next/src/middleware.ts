fc782ab36b1be863d0d2d4c25e484afe233c64f8
Redish101 <i@redish101.top>
2025-02-04 13:41:35 +0800

    feat(middleware): 根据环境变量动态设置访问中间件的服务器 URL

--- apps/example-next/src/middleware.ts
+++ apps/example-next/src/middleware.ts
@@ -1,6 +1,11 @@
 import { redish101AccessMiddleware } from "@redish101-access/nextjs"
 
-export const middleware = redish101AccessMiddleware("http://localhost:3000")
+const serverUrl =
+    process.env.NODE_ENV === 'production'
+        ? 'https://access.redish101.top'
+        : 'http://localhost:3000'
+
+export const middleware = redish101AccessMiddleware(serverUrl)
 
 export const config = {
     matcher: '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
